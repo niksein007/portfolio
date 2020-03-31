13ce07af7c7fd3c5da5d4a9762017d7014fff24a
niksein007 <markpearceniksein@gmail.com>
2020-03-31 21:38:39 +0100

    Updates

---
+++ precache-manifest.d356fad6ff1fec9857efa526f4c7064c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4058fb9a1027a7cc8204d06536c4edd7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b4f2013f97401fd6fd5c",
+    "url": "/portfolio/static/css/main.53ac038b.chunk.css"
+  },
+  {
+    "revision": "31f4f56cc5226028586a",
+    "url": "/portfolio/static/js/2.a13ccc6a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio/static/js/2.a13ccc6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f2013f97401fd6fd5c",
+    "url": "/portfolio/static/js/main.e0b87640.chunk.js"
+  },
+  {
+    "revision": "7107900f59be1fd95810",
+    "url": "/portfolio/static/js/runtime-main.af0b07c5.js"
+  },
+  {
+    "revision": "7356b79f6eb2ba8e706d84fa2f280868",
+    "url": "/portfolio/static/media/bulbs.7356b79f.jpg"
+  },
+  {
+    "revision": "10e8c61563d820e5e53035064162dfe8",
+    "url": "/portfolio/static/media/bulbs2.10e8c615.jpg"
+  },
+  {
+    "revision": "c4eff4fa0d929f23f79639d5ace4d4b3",
+    "url": "/portfolio/static/media/dice.c4eff4fa.jpg"
+  },
+  {
+    "revision": "ca2c2391cb67b5dd86b6d28443edddd7",
+    "url": "/portfolio/static/media/instagram.ca2c2391.jpg"
+  }
+]);
